20ecb80f122b9c3cc4960af6541d1350efa277df
José Fresco <josemariafresco@gmail.com>
2017-07-14 12:50:06 -0300

    Add allTopStoriesLoader

--- ex/05/src/schema/index.js
+++ ex/05/src/schema/index.js
@@ -76,13 +76,13 @@ const resolvers = {
 
 const itemLoader = new DataLoader(ids => Promise.all(ids.map(getItem)))
 
-const topStoriesLoader = new DataLoader(async params => Promise.all(params.map(async ({ page, count } = {}) => {
-  const stories = await getTopStories()
-  if (page && count) {
-    return itemLoader.loadMany(stories.slice(page * count, (page * count) + count))
-  }
+const allTopStoriesLoader = new DataLoader(keys => Promise.all(keys.map(getTopStories)))
 
-  return itemLoader.loadMany(stories)
+const topStoriesLoader = new DataLoader(async params => Promise.all(params.map(async ({ page, count } = {}) => {
+  const stories = await allTopStoriesLoader.load('all')
+  return itemLoader.loadMany(page && count
+    ? stories.slice(page * count, (page * count) + count)
+    : stories)
 })))
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers })
