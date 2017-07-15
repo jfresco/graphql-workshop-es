bc4f8f339848b703ded1cd15301eea57afb744ea
José Fresco <josemariafresco@gmail.com>
2017-07-15 08:49:35 -0300

    Add CORS

--- ex/05/src/index.js
+++ ex/05/src/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const { graphqlExpress, graphiqlExpress } = require('graphql-server-express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const schema = require('./schema')
 
 const app = express()
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
+app.use('/graphql', cors(), bodyParser.json(), graphqlExpress({ schema }))
+
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
-app.listen(3000, function () {
-  console.log('Our Node server is up and running on port 3000!')
+app.listen(3001, function () {
+  console.log('Our Node server is up and running on port 3001!')
 })
